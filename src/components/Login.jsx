cb24f070710796d270fd102f6dfaf43756d78517
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-27 16:19:01 +0600

    login functionality done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,71 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (username.trim() === "" || password.trim() === "") {
+      setError("All fields are required");
+      return;
+    }
+    
+    const res = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await res.json();
+    console.log(data);
+    if (data.token) {
+      localStorage.setItem("loginToken", data.token);
+      navigate("/");
+    } else {
+      setError(data.message);
+    }
+  };
+
   return (
-    <div>
-      <div className="flex justify-center items-center mt-20">
-        <form className="shad p-10 rounded-lg">
+    <div className="mt-20">
+      {error && (
+        <h2 className="text-center text-red-500 font-[500]">{error}</h2>
+      )}
+      <h2 className="text-center my-5 font-[500]">
+        Do not have an account ?
+        <span className="bg-indigo-600 text-white ml-2 px-2 py-1 border rounded ">
+          <Link to="/signup"> Register</Link>
+        </span>
+      </h2>
+      <div className="flex justify-center items-center ">
+        <form className="shad p-10 rounded-lg" onSubmit={handleLogin}>
           <input
             type="text"
             className="px-5 py-2 rounded bg-slate-300 placeholder-black"
             name="username"
             placeholder="Enter Username"
+            onChange={(e) => setUsername(e.target.value)}
           />
           <br />
           <input
-            type="text"
+            type="password"
             className="px-5 py-2 rounded bg-slate-300 my-5 placeholder-black"
             name="password"
             placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <br />
           <input
             type="button"
             value="Login"
-            className="px-5 py-2 rounded bg-indigo-700 hover:bg-indigo-800 text-white font-bold"
+            className="px-5 py-2 rounded bg-indigo-700 hover:bg-indigo-800 text-white font-bold cursor-pointer "
+            onClick={handleLogin}
           />
         </form>
       </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Signup() {
   return (
-    <div>
-      <div className="flex justify-center items-center mt-20">
+    <div className="mt-20">
+      <h2 className="text-center my-5 font-[500]">
+        Already have an account ?
+        <span className="bg-indigo-600 text-white ml-2 px-2 py-1 border rounded">
+          <Link to="/login"> Sign In</Link>
+        </span>
+      </h2>
+      <div className="flex justify-center items-center ">
         <form className="shad p-10 rounded-lg">
           <input
             type="text"
@@ -12,14 +20,14 @@ export default function Signup() {
           <br />
           <input
             type="text"
-            className="px-5 py-2 rounded bg-slate-300 placeholder-black"
+            className="px-5 py-2 rounded bg-slate-300 placeholder-black my-5"
             name="username"
             placeholder="Enter Username"
           />
           <br />
           <input
             type="text"
-            className="px-5 py-2 rounded bg-slate-300 my-5 placeholder-black"
+            className="px-5 py-2 rounded bg-slate-300  placeholder-black"
             name="password"
             placeholder="Enter Password"
           />
@@ -27,7 +35,7 @@ export default function Signup() {
           <input
             type="button"
             value="Create Account"
-            className="px-5 py-2 rounded bg-indigo-700 hover:bg-indigo-800 text-white font-bold"
+            className="my-5 px-5 py-2 rounded bg-indigo-700 hover:bg-indigo-800 text-white font-bold"
           />
         </form>
       </div>
