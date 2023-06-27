88306752bc67568339035c5b2fd99f0d8c260d3c
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-27 16:54:51 +0600

    profile and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Reset from "./components/Reset";
 import Signup from "./components/Signup";
 
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
       <Route element={<Home />} path="/" />
       <Route element={<Login />} path="/login" />
       <Route element={<Signup />} path="/signup" />
+      <Route element={<Profile />} path="/profile" />
+      <Route element={<Reset />} path="/reset" />
     </Routes>
   );
 }
--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,63 +1,69 @@
 import { useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
-import { useProvider } from "../contextAPI/context";
 
-export default function Login() {
+export default function Reset() {
   const navigate = useNavigate();
-  const { loggedUser, setLoggedUser } = useProvider();
   const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  console.log(loggedUser);
-  const handleLogin = async (e) => {
+  const handleSignup = async (e) => {
     e.preventDefault();
 
-    if (username.trim() === "" || password.trim() === "") {
+    if (
+      username.trim() === "" ||
+      password.trim() === "" ||
+      email.trim() === ""
+    ) {
       setError("All fields are required");
       return;
     }
+    if (password.length < 6) {
+      setError("Password must be at least 6 characters");
+      return;
+    }
 
-    const res = await fetch("http://localhost:5000/api/login", {
+    const res = await fetch("http://localhost:5000/api/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ email, username, password }),
     });
     const data = await res.json();
     console.log(data);
-    if (data.token) {
+    if (data.status === "success") {
       localStorage.setItem("loginToken", data.token);
-      setLoggedUser(data.data);
-      toast.success("Login Successfull navigating to home page");
-      setTimeout(() => {
-        navigate("/");
-      }, 2000);
+      navigate("/login");
     } else {
-      toast.error("Invalid Credentials");
       setError(data.message);
     }
   };
 
   return (
     <div className="mt-20">
-      <Toaster />
       {error && (
         <h2 className="text-center text-red-500 font-[500]">{error}</h2>
       )}
       <h2 className="text-center my-5 font-[500]">
-        Do not have an account ?
-        <span className="bg-indigo-600 text-white ml-2 px-2 py-1 border rounded ">
-          <Link to="/signup"> Register</Link>
+        Already have an account ?
+        <span className="bg-indigo-600 text-white ml-2 px-2 py-1 border rounded">
+          <Link to="/login"> Sign In</Link>
         </span>
       </h2>
-
       <div className="flex justify-center items-center ">
-        <form className="shad p-10 rounded-lg" onSubmit={handleLogin}>
+        <form className="shad p-10 rounded-lg" onSubmit={handleSignup}>
           <input
             type="text"
             className="px-5 py-2 rounded bg-slate-300 placeholder-black"
+            name="email"
+            placeholder="Enter Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            className="px-5 py-2 rounded bg-slate-300 placeholder-black my-5"
             name="username"
             placeholder="Enter Username"
             onChange={(e) => setUsername(e.target.value)}
@@ -65,7 +71,7 @@ export default function Login() {
           <br />
           <input
             type="password"
-            className="px-5 py-2 rounded bg-slate-300 my-5 placeholder-black"
+            className="px-5 py-2 rounded bg-slate-300  placeholder-black"
             name="password"
             placeholder="Enter Password"
             onChange={(e) => setPassword(e.target.value)}
@@ -75,16 +81,10 @@ export default function Login() {
             type="button"
             value="Login"
             className="px-5 py-2 rounded bg-indigo-700 hover:bg-indigo-800 text-white font-bold cursor-pointer "
-            onClick={handleLogin}
+            onClick={handleSignup}
           />
         </form>
       </div>
-      <h2 className="text-center my-5 font-[500]">
-        Forgot Password ?
-        <span className="bg-red-600 text-white ml-2 px-2 py-1 border rounded ">
-          <Link to="/reset"> Reset</Link>
-        </span>
-      </h2>
     </div>
   );
 }
